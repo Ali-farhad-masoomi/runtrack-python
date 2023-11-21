ef7673008dd212066886a850e93d5ff4ace0e7b0
Ali-farhad-masoomi <146720078+Ali-farhad-masoomi@users.noreply.github.com>
2023-11-21 12:12:00 +0100

    Add files via upload

---
+++ jour01/Job06/chaine.py
@@ -0,0 +1,6 @@
+# on définit une variable
+ma_string = "Je suis une String"
+
+# On affiche la variable
+
+print(ma_string)
---
+++ jour01/Job07/somme.py
@@ -0,0 +1,12 @@
+# On déclare deux variables
+
+num1 = 40
+num2 = 2
+
+# on déclare une variable somme
+
+somme = num1 + num2
+
+# On affiche la varibale somme
+
+print("Le résultat est :", somme)
